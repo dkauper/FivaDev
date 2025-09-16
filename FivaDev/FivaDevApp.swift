059ebb027726b2b4628b5f4551b00698726494ed
Doron Kauper <doron@kauper.us>
2025-09-16 06:06:42 -0700

    Initial Commit

---
+++ FivaDev/FivaDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  FivaDevApp.swift
+//  FivaDev
+//
+//  Created by Doron Kauper on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FivaDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
